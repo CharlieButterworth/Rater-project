79b78e669f9aff823003b2044e58fe381b2a76d6
Charles Butterworth <charlessbutterworth@gmail.com>
2021-02-17 13:59:02 -0600

    have games rendering in correct route

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include
+from rest_framework import routers
 from django.urls import path
 from raterapi.views import register_user, login_user
+from raterapi.views import GamesView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', GamesView, 'game')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
